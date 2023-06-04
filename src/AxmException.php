b96c372c30a130ab5bea097ce4b0fcf129161362
juancristobalgd1 <65052633+juancristobalgd1@users.noreply.github.com>
2023-06-04 22:16:03 +0200

    Add files via upload

--- src/AxmException.php
+++ src/AxmException.php
@@ -41,6 +41,14 @@ class AxmException extends \Exception
     protected static $view404 = ['no_controller', 'no_action', 'num_params', 'no_view', 'no_route'];
 
 
+    private const HIGHLIGHT_COMMENT_COLOR = '#008000';
+
+    private const HIGHLIGHT_HTML_COLOR = '#808080';
+
+    private const HIGHLIGHT_KEYWORD_COLOR = '#9406adc0';
+
+    private const HIGHLIGHT_STRING_COLOR = '#DD0000';
+
     /** Cantidad de linea de codigo a mostrar  
      * @var int
      */
@@ -156,8 +164,8 @@ class AxmException extends \Exception
 
         $response = null;
         if (OpenAIChatbot::$activate) {
-            $prompt = self::convertToString($data);
-            $chatbot = OpenAIChatbot::getInstance($config);
+            $prompt   = self::convertToString($data);
+            $chatbot  = OpenAIChatbot::getInstance($config);
             $response = $chatbot->getResponse($prompt);
 
             return $response;
@@ -234,40 +242,142 @@ class AxmException extends \Exception
      * @param integer $maxLines maximum number of lines to display
      * @return string the rendering result
      */
-    protected static function renderSourceCode($file, $errorLine, $maxLines)
+    // protected static function renderSourceCode2($file, $errorLine, $maxLines)
+    // {
+    //     $errorLine--;    // adjust line number to 0-based from 1-based
+    //     if ($errorLine < 0 || ($lines = @file($file)) === false || ($lineCount = count($lines)) <= $errorLine)
+    //         return '';
+
+    //     self::configureHighlightColors();
+
+    //     $halfLines  = (int)($maxLines / 2);
+    //     $beginLine  = $errorLine - $halfLines > 0 ? $errorLine - $halfLines : 0;
+    //     $endLine    = $errorLine + $halfLines < $lineCount ? $errorLine + $halfLines : $lineCount - 1;
+    //     $lineNumberWidth = strlen($endLine + 1);
+
+    //     $output = '';
+    //     for ($i = $beginLine; $i <= $endLine; ++$i) {
+    //         $isErrorLine = $i === $errorLine;
+    //         $lineI = static::highlight_code(htmlspecialchars_decode(str_replace(["\r", "\n", "\t"], [''], $lines[$i])));
+    //         $code  = sprintf("<span class=\"ln" . ($isErrorLine ? ' error-ln' : '') . "\">%0{$lineNumberWidth}d</span> %s", $i + 1, str_replace(["\n", "\t", "\v"], [''], ($lineI == '') ? $lineI : $lineI . '</br>'));
+    //         if (!$isErrorLine)
+    //             $output .= $code;
+    //         else
+    //             $output .= '<span class="error">' . $code . '</span>';
+    //     }
+
+    //     return '<div class="code"><pre>' . $output . '</pre></div>';
+    // }
+
+
+
+    /**
+     * Renders the source code by highlighting the error line and displaying a range of lines.
+     *
+     * @param string $file Source code file.
+     * @param int $errorLine Error line number.
+     * @param int $maxLines (optional) Maximum number of lines to display. Default value: 10.
+     * @return string HTML code with highlighted source code.
+     */
+    protected static function renderSourceCode($file, $errorLine, $maxLines = 10)
     {
-        // Set our highlight colors:
-        if (function_exists('ini_set')) {
-            ini_set('highlight.comment', '#008000; font-style: italic');
-            ini_set('highlight.html', '#808080');
-            ini_set('highlight.keyword', '#9406adc0; font-weight: bold');  //cc66cc
-            ini_set('highlight.string', '#DD0000');
+        $errorLine--; // Adjust line number to 0-based from 1-based
+        if ($errorLine < 0 || ($lines = @file($file)) === false || ($lineCount = count($lines)) <= $errorLine) {
+            return '';
         }
 
-        $errorLine--;    // adjust line number to 0-based from 1-based
-        if ($errorLine < 0 || ($lines = @file($file)) === false || ($lineCount = count($lines)) <= $errorLine)
-            return '';
+        self::configureHighlightColors();
 
-        $halfLines  = (int)($maxLines / 2);
-        $beginLine = $errorLine - $halfLines > 0 ? $errorLine - $halfLines : 0;
-        $endLine = $errorLine + $halfLines < $lineCount ? $errorLine + $halfLines : $lineCount - 1;
-        $lineNumberWidth = strlen($endLine + 1);
+        $lineRange = self::calculateLineRange($errorLine, $lineCount, $maxLines);
+        $highlightedLines = self::highlightLines($lines, $lineRange, $errorLine);
 
+        return self::generateHTML($highlightedLines, $lineRange['begin'], $lineRange['end'], $errorLine);
+    }
+
+    /**
+     * Calculates the line range to display.
+     *
+     * @param int $errorLine Error line number.
+     * @param int $lineCount Total number of lines.
+     * @param int $maxLines Maximum number of lines to display.
+     * @return array Line range to display (start and end).
+     */
+    protected static function calculateLineRange($errorLine, $lineCount, $maxLines)
+    {
+        $halfLines = (int) ($maxLines / 2);
+        $beginLine = max($errorLine - $halfLines, 0);
+        $endLine   = min($beginLine + $maxLines - 1, $lineCount - 1);
+
+        return [
+            'begin' => $beginLine,
+            'end'   => $endLine
+        ];
+    }
+
+    /**
+     * Highlights the relevant lines of the source code.
+     *
+     * @param array $lines Array of code lines.
+     * @param array $lineRange Line range to display.
+     * @param int $errorLine Error line number.
+     * @return array Array of highlighted lines.
+     */
+    protected static function highlightLines($lines, $lineRange, $errorLine)
+    {
+        $highlightedLines = [];
+        foreach ($lines as $i => $line) {
+            if ($i < $lineRange['begin'] || $i > $lineRange['end']) {
+                continue;
+            }
+
+            $highlightedLines[] = static::highlight_code(htmlspecialchars_decode(str_replace(["\r", "\n", "\t"], [''], $line)), $i === $errorLine);
+        }
+
+        return $highlightedLines;
+    }
+
+    /**
+     * Generates the HTML code with the highlighted source code.
+     *
+     * @param array $highlightedLines Array of highlighted lines.
+     * @param int $beginLine Start line.
+     * @param int $endLine End line.
+     * @param int $errorLine Error line number.
+     * @return string HTML code with the highlighted source code.
+     */
+    protected static function generateHTML($highlightedLines, $beginLine, $endLine, $errorLine)
+    {
         $output = '';
-        for ($i = $beginLine; $i <= $endLine; ++$i) {
-            $isErrorLine = $i === $errorLine;
-            $lineI = static::highlight_code(htmlspecialchars_decode(str_replace(["\r", "\n", "\t"], [''], $lines[$i])));
-            $code = sprintf("<span class=\"ln" . ($isErrorLine ? ' error-ln' : '') . "\">%0{$lineNumberWidth}d</span> %s", $i + 1, str_replace(["\n", "\t", "\v"], [''], ($lineI == '') ? $lineI : $lineI . '</br>'));
-            if (!$isErrorLine)
-                $output .= $code;
-            else
-                $output .= '<span class="error">' . $code . '</span>';
+        $lineNumberWidth = strlen($endLine + 1);
+        foreach ($highlightedLines as $i => $lineI) {
+            $lineNumber  = $i + $beginLine + 1;
+            $isErrorLine = $lineNumber - 1 === $errorLine;
+            $code = sprintf(
+                '<span class="ln%s">%0' . $lineNumberWidth . 'd</span> %s',
+                ($isErrorLine ? ' error-ln' : ''),
+                $lineNumber,
+                str_replace(["\n", "\t", "\v"], [''], ($lineI == '') ? $lineI : $lineI . '</br>')
+            );
+
+            $output .= $isErrorLine ? '<span class="error">' . $code . '</span>' : $code;
         }
 
         return '<div class="code"><pre>' . $output . '</pre></div>';
     }
 
 
+    /**
+     * Configures the highlight colors for the source code.
+     */
+    protected static function configureHighlightColors()
+    {
+        if (function_exists('ini_set')) {
+            ini_set('highlight.comment', '#008000; font-style: italic');
+            ini_set('highlight.html', '#808080');
+            ini_set('highlight.keyword', '#9406adc0; font-weight: bold'); // cc66cc
+            ini_set('highlight.string', '#DD0000');
+        }
+    }
 
     /**
      * Returns a value indicating whether the call stack is from application code.
@@ -285,51 +395,70 @@ class AxmException extends \Exception
     }
 
 
+    /**
+     * Converts an array of arguments to a string representation.
+     *
+     * @param array $args Array of arguments.
+     * @return string String representation of the arguments.
+     */
     protected static function argumentsToString($args)
     {
-        $count = 0;
-        $isAssoc = $args !== array_values($args);
         $output = '';
 
         foreach ($args as $key => $value) {
-            if ($count >= self::$maxTraceSourceLines) {
-                if ($count > self::$maxStrlengTraceFile)
-                    break;
+            if (strlen($output) > self::$maxStrlengTraceFile) {
                 $output .= '...';
-                continue;
+                break;
             }
 
             $arg = '';
-            if (is_object($value))
-                $arg = get_class($value);
-            elseif (is_bool($value))
-                $arg = $value ? 'true' : 'false';
-            elseif (is_string($value)) {
-                if (strlen($value) > self::$maxStrlengTraceFile)
-                    $arg = '"' . substr($value, 0, self::$maxStrlengTraceFile) . '..."';
-                else
-                    $arg = '"' . $value . '"';
-            } elseif (is_array($value))
-                $arg = 'array(' . self::argumentsToString($value) . ')';
-            elseif ($value === null)
-                $arg = 'null';
-            elseif (is_resource($value))
-                $arg = 'resource';
 
-            if (is_string($key))
+            if (is_object($value)) {
+                $arg = get_class($value);
+            } elseif (is_bool($value)) {
+                $arg = $value ? 'true' : 'false';
+            } elseif (is_string($value)) {
+                $arg = self::truncateString($value, self::$maxStrlengTraceFile);
+            } elseif (is_array($value)) {
+                $arg = 'array(' . self::argumentsToString($value) . ')';
+            } elseif ($value === null) {
+                $arg = 'null';
+            } elseif (is_resource($value)) {
+                $arg = 'resource';
+            }
+
+            if (is_string($key)) {
                 $arg = '"' . $key . '" => ' . $arg;
-            elseif ($isAssoc)
-                $arg = $key . ' => ' . $arg;
+            } elseif (is_int($key)) {
+                $arg = $arg;
+            }
 
             $output .= $arg;
-            $count++;
 
-            if ($count < count($args))
+            if (next($args)) {
                 $output .= ', ';
+            }
         }
 
         return $output;
     }
+
+    /**
+     * Truncates a string to a specified length.
+     *
+     * @param string $string The string to truncate.
+     * @param int $length The maximum length of the truncated string.
+     * @return string The truncated string.
+     */
+    protected static function truncateString($string, $length)
+    {
+        if (strlen($string) > $length) {
+            $string = substr($string, 0, $length) . '...';
+        }
+        return '"' . $string . '"';
+    }
+
+
 
     /** The highlight string function encodes and highlights
      * brackets so we need them to start raw.
@@ -340,9 +469,9 @@ class AxmException extends \Exception
      */
     protected static function highlight_code(string $str): string
     {
-        $search = ['&lt;', '&gt;', '<?', '?>', '<%', '%>', '\\', '</script>'];
+        $search  = ['&lt;', '&gt;', '<?', '?>', '<%', '%>', '\\', '</script>'];
         $replace = ['<', '>', 'phptagopen', 'phptagclose', 'asptagopen', 'asptagclose', 'backslashtmp', 'scriptclose'];
-        $str = strtr($str, array_combine($search, $replace));
+        $str     = strtr($str, array_combine($search, $replace));
 
         // The highlight_string function requires that the text be surrounded
         // by PHP tags, which we will remove later
@@ -362,9 +491,9 @@ class AxmException extends \Exception
             $str
         );
 
-        $search = ['phptagopen', 'phptagclose', 'asptagopen', 'asptagclose', 'backslashtmp', 'scriptclose'];
+        $search  = ['phptagopen', 'phptagclose', 'asptagopen', 'asptagclose', 'backslashtmp', 'scriptclose'];
         $replace = ['&lt;?', '?&gt;', '&lt;%', '%&gt;', '\\', '&lt;/script&gt;'];
-        $str = strtr($str, array_combine($search, $replace));
+        $str     = strtr($str, array_combine($search, $replace));
 
         return $str;
     }
